a67a2dcc1e9f16d148ee620062128467abdf8231
baobaomi900901 <baobaomi900901@icloud.com>
2022-04-17 16:46:12 +0800

    06-粒子连线

--- src/粒子模板-class/script.js
+++ src/粒子模板-class/script.js
@@ -8,25 +8,34 @@ ctx.canvas.height = window.innerHeight;
 let particles = [];
 
 // 2.创建一个粒子构造函数
-function Particle() {
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
 
-}
+    // 3.给粒子构造函数添加一个绘制方法,
+    draw() {
+        ctx.beginPath();
+    }
 
-// 3.给粒子构造函数添加一个绘制方法,
-Particle.prototype.draw = function () {
-}
-
-// 4.给粒子构造函数添加一个更新方法
-Particle.prototype.update = function () {
-    // 判断粒子是否超出边界
-
-    // 更新时偏移
-    this.draw()
+    // 4.给粒子构造函数添加一个更新方法
+    update() {
+        // 重绘粒子
+        this.draw()
+    }
 }
 
 function init() {
+    // 创建粒子
     particles = [];
-    for (let i = 0; i < 100; i++) {
+    // 粒子数量
+    let numberOfParticles = 100;
+    for (let i = 0; i < numberOfParticles; i++) {
     }
 }
 
@@ -34,11 +43,9 @@ function init() {
 // 递归动画
 function animate() {
     requestAnimationFrame(animate);
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
+        // particles[i].update();
     }
 }
 init()
